59ab1848c0c73bde3ade45f2152783c0e4b92fbf
RahulPraj <rahul971801@gmail.com>
2025-04-01 10:33:41 +0530

    project completed

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleEcom')
+
+let MONGODB_URL = 'mongodb+srv://rahul971801:aVlZqkwZWsbcBfpq@cluster0.u5kvypq.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log('db connected');
 }).catch((err)=>{
